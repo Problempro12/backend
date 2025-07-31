a01a01e3bc98b417cb2afbc8d9c08b07bb8bce58
problempro12 <alexankochergin@yandex.ru>
2025-07-31 21:10:53 +0300

    1.5

--- QuickPoll/urls.py
+++ QuickPoll/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/polls/', include('polls.urls')),
+    path('api/poll/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
